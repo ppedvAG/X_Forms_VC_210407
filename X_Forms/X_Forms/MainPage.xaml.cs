9e77f63c77bb22aef132a35ecc1c42a9189ee029
KlaasHagge <klaash@ppedv.de>
2021-04-07 15:15:58 +0200

    Bindings

--- X_Forms/X_Forms/MainPage.xaml.cs
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace X_Forms
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person(){Vorname="Anna", Nachname="Nass"}
+        };
+
         public MainPage()
         {
             //Initialisierung der UI (Xaml-Datei). Sollte immer erste Aktion des Konstruktors sein
@@ -17,12 +23,16 @@ namespace X_Forms
 
             //Neuzuweisung einer UI-Property über die x:Name-Property des Steuerelements
             Lbl_Main.Text = "Hallo Xamarin";
+
+            this.BindingContext = this;
         }
 
         private void Btn_KlickMich_Clicked(object sender, EventArgs e)
         {
             //Neuzuweisung einer Property des Eventauslösenden Steuerelements
             (sender as Button).Text = "Wurde geklickt";
+
+            Personenliste.Add(new Person() { Vorname = Ent_Vorname?.Text, Nachname = Ent_Nachname?.Text });
         }
 
         private async void IBtn_Beispiel_Clicked(object sender, EventArgs e)
@@ -32,7 +42,12 @@ namespace X_Forms
 
             if (erg)
                 //Neuzuweisung der Textproperty des Labeld mit dem Ausgewählten Element des Pickers
-                Lbl_Output.Text = Pkr_Monkeys.SelectedItem?.ToString();
+                Lbl_Main.Text = Pkr_Monkeys.SelectedItem?.ToString();
+        }
+
+        private void Btn_Name_Clicked(object sender, EventArgs e)
+        {
+            (StL_DataBinding.BindingContext as Person).Vorname = "Martin";
         }
     }
 }
