70012da326cbc5d6027368d6d51b32711e2a1650
KlaasHagge <klaash@ppedv.de>
2021-04-07 16:43:07 +0200

    Tag 2 Abschluss (kommentiert)

--- X_Forms/X_Forms/App.xaml.cs
+++ X_Forms/X_Forms/App.xaml.cs
@@ -6,13 +6,17 @@ namespace X_Forms
 {
     public partial class App : Application
     {
+        //Die App-Klasse beinhaltet eine grundlegen Initialisierung der App sowie die MainPage-Property, welche defniert,
+        //welche Page gerade in der App angezeigt wird. Diese Property wird auch als Einstiegspunkt verwendet.
         public App()
         {
             InitializeComponent();
 
-            MainPage = new Layouts.StackLayoutBsp();
+            //Zuweisung der MainPage-Property zu einer Page
+            MainPage = new MainPage();
         }
 
+        //Methoden, welche zu bestimmten globalen Events ausgeführt werden (Start, Unterbrechen der App [Sleep], Wiederaktivierung der App [Resume])
         protected override void OnStart()
         {
         }
