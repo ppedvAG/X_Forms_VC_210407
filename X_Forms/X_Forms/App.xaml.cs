dbe3046eb7864ef93d30c62f26f5039cac7c40f4
KlaasHagge <klaash@ppedv.de>
2021-04-07 16:19:41 +0200

    Layouts (kommentiert)

--- X_Forms/X_Forms/App.xaml.cs
+++ X_Forms/X_Forms/App.xaml.cs
@@ -10,7 +10,7 @@ namespace X_Forms
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Layouts.StackLayoutBsp();
         }
 
         protected override void OnStart()
--- X_Forms/X_Forms/MainPage.xaml.cs
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -11,11 +11,14 @@ namespace X_Forms
 {
     public partial class MainPage : ContentPage
     {
+        //Property zum Zwischenspeichern der Personenliste (ObservableCollection ist eine generische Liste, welche die GUI
+        //automatisch über Veränderungen innerhalb der Liste informiert)
         public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
         {
             new Person(){Vorname="Anna", Nachname="Nass"}
         };
 
+        //Konstruktor
         public MainPage()
         {
             //Initialisierung der UI (Xaml-Datei). Sollte immer erste Aktion des Konstruktors sein
@@ -24,6 +27,8 @@ namespace X_Forms
             //Neuzuweisung einer UI-Property über die x:Name-Property des Steuerelements
             Lbl_Main.Text = "Hallo Xamarin";
 
+            //Durch Setzen des BindingContextes nehmen Kurzbindungen aus dem XAML-Code automatisch Bezug auf die Properties
+            //des im BindingContext gesetzten Objekts
             this.BindingContext = this;
         }
 
@@ -47,6 +52,7 @@ namespace X_Forms
 
         private void Btn_Name_Clicked(object sender, EventArgs e)
         {
+            //Änderung einer Property des BindingContexts des StackLayouts (INotifyPropertyChanged informiert GUI über Veränderung (vgl. Person.cs))
             (StL_DataBinding.BindingContext as Person).Vorname = "Martin";
         }
     }
